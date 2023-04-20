9ea35db21c090524f0f0f1d6ce6ada61ccfca414
Jakub Przybyla <jakub.przybyla@nanovo.pl>
2023-04-20 16:11:16 +0200

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup></script>
 
-<template> test </template>
+<template>
+	<div class="header fw-bold d-flex align-items-center h-auto w-100">Shopware listing</div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	.header {
+		position: absolute;
+		top: 0;
+		left: 0;
+		padding: 10px 100px;
+		background-color: #000000;
+		color: #ffffff;
+	}
+</style>
